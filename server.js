7bd81419d6721f7b0916a62a73a4ac908d222e20
khang1244 <96627298+khang1244@users.noreply.github.com>
2025-03-05 18:42:14 +0700

    Cai dat express chay thong bao xin chao

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require("./app");
+const config = require("./app/config");
+
+// start server
+const PORT = config.app.port;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
